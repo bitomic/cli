5cfb8c43747e7da233a65829b7bddc001e074b8e
bitomic <35199700+bitomic@users.noreply.github.com>
2022-02-21 17:09:25 -0600

    fix: register pieces and emit ready event

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -1,6 +1,9 @@
+import { CommandStore, ListenerStore } from './structures/index.js'
 import { type Container, container } from '@sapphire/pieces'
-import { CommandStore } from './structures/index.js'
 import { EventEmitter } from 'events'
+import { Events } from './types/index.js'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 export class Client extends EventEmitter {
 	public readonly container: Container
@@ -9,7 +12,17 @@ export class Client extends EventEmitter {
 		super()
 		this.container = container
 		this.container.client = this
-		container.stores.register( new CommandStore() )
+
+		const basepath = path.resolve( fileURLToPath(import.meta.url), '../..' )
+		container.stores.register( new CommandStore().registerPath( path.resolve( basepath, 'commands' ) ) )
+		container.stores.register( new ListenerStore().registerPath( path.resolve( basepath, 'listeners' ) ) )
+	}
+
+	public async start(): Promise<void> {
+		for ( const store of this.container.stores.values() ) {
+			await store.loadAll()
+		}
+		this.emit( Events.Ready )
 	}
 }
 
